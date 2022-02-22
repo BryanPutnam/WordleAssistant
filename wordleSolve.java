79ccc4d231b399b0dd3b1babd5c42fab56e058aa
Bryan Putnam <bryanputnam25@gmail.com>
2022-02-22 08:24:41 -0600

    supposed scanner redundancy fix. removeLetter method throwing IllegalStateException

--- wordleSolve.java
+++ wordleSolve.java
@@ -5,12 +5,13 @@ import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-class wordleSolve { 
+class wordleSolve {  
+
+    public static Scanner scan = new Scanner(System.in); 
 
     public static void numLetter(ArrayList<String> arrlst) { 
-        Scanner usrIn = new Scanner(System.in); 
         System.out.println("How many letters do you know are in the word?"); 
-        String input1 = usrIn.nextLine(); 
+        String input1 = scan.nextLine(); 
         int x = Integer.parseInt(input1); 
         System.out.println("What letters do you know?");
         ArrayList<String> newWords = new ArrayList<>();
@@ -20,11 +21,34 @@ class wordleSolve {
             case 3: threeLetter(arrlst, newWords);    break; 
             case 4: fourLetter(arrlst, newWords);     break; 
         }
-        usrIn.close(); 
+    }
+
+    public static void removeLetter(ArrayList<String> arrlst, ArrayList<String> newWords) { 
+        System.out.println("How many letters will not work?");
+        if (!scan.hasNextLine()) { 
+            System.out.println("no next line"); 
+        }
+        String num = scan.nextLine(); 
+        int n = Integer.parseInt(num); 
+        System.out.println("What letters will not work?"); 
+        String[] arrNot = new String[n+1]; 
+        ArrayList<String> toRemove = new ArrayList<>(); 
+        for(int i = 0; i < arrNot.length; i++) { 
+            arrNot[i] = scan.nextLine(); 
+        }
+        scan.close(); 
+        for(String i : newWords) { 
+            for(String j : arrNot) { 
+                if(i.contains(j)) { 
+                    toRemove.add(i); 
+                }
+            }
+        }
+        newWords.removeAll(toRemove); 
+        System.out.println(newWords); 
     }
 
     public static void oneLetter(ArrayList<String> arrlst, ArrayList<String> newWords) { 
-        Scanner scan = new Scanner(System.in); 
         String oneLet = scan.nextLine();  
         for(String s : arrlst) { 
             if(s.contains(oneLet)) { 
@@ -32,26 +56,24 @@ class wordleSolve {
             }
         }
         scan.close(); 
-        System.out.println(newWords); 
+        removeLetter(arrlst, newWords); 
     }
 
     public static void twoLetter(ArrayList<String> arrlst, ArrayList<String> newWords) { 
-        Scanner scan = new Scanner(System.in); 
         String[] arr = new String[2]; 
         for(int i = 0; i < arr.length; i++) { 
             arr[i] = scan.nextLine(); 
         }
         for(String s : arrlst) { 
             if(s.contains(arr[0]) && (s.contains(arr[1]))) { 
-                newWords.add(s); 
+                newWords.add(s);  
             }
         }
         scan.close(); 
-        System.out.println(newWords);
+        removeLetter(arrlst, newWords);
     }
 
     public static void threeLetter(ArrayList<String> arrlst, ArrayList<String> newWords) { 
-        Scanner scan = new Scanner(System.in); 
         String[] arr = new String[3]; 
         for(int i = 0; i < arr.length; i++) { 
             arr[i] = scan.nextLine(); 
@@ -62,11 +84,10 @@ class wordleSolve {
             }
         }
         scan.close(); 
-        System.out.println(newWords);
+        removeLetter(arrlst, newWords);
     }
 
     public static void fourLetter(ArrayList<String> arrlst, ArrayList<String> newWords) { 
-        Scanner scan = new Scanner(System.in); 
         String[] arr = new String[4]; 
         for(int i = 0; i < arr.length; i++) { 
             arr[i] = scan.nextLine(); 
@@ -77,14 +98,14 @@ class wordleSolve {
             }
         }
         scan.close(); 
-        System.out.println(newWords);
+        removeLetter(arrlst, newWords);
     }
 
     public static void main(String[] args) throws FileNotFoundException { 
-        Scanner scan = new Scanner(new File("/Users/bryanputnam/Desktop/School/VSC_Files/Java/wordle.txt"));  
+        Scanner fileScan = new Scanner(new File("/Users/bryanputnam/Desktop/School/VSC_Files/Java/wordle.txt"));  
         ArrayList<String> arrlst = new ArrayList<>(); 
-        while (scan.hasNext()) { 
-            arrlst.add(scan.next()); 
+        while (fileScan.hasNext()) { 
+            arrlst.add(fileScan.next()); 
         }
         numLetter(arrlst);
     }
